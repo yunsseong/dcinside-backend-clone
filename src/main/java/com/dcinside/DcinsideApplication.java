8e957a362c55d5e72eb37ab88d64d1fea75d43fc
yunsseong <aster20@hanyang.ac.kr>
2024-01-22 15:05:30 +0900

    최초 커밋

---
+++ src/main/java/com/dcinside/DcinsideApplication.java
@@ -0,0 +1,13 @@
+package com.dcinside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcinsideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcinsideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcinside/DcinsideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcinside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcinsideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
